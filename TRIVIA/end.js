ccf1b19505df17fc4256d015b9ec45dc7ea678c7
srishtiii03 <87142109+srishtiii03@users.noreply.github.com>
2021-07-10 13:04:57 +0530

    Update end.js

--- TRIVIA/end.js
+++ TRIVIA/end.js
@@ -25,5 +25,5 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('https://srishtiii03.github.io/');
 };
